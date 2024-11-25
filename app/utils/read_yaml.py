c2633e3c8fc2df446162702157a776e0a16e47b3
AJlekCandr_proff <svznikov@yandex.ru>
2024-11-25 13:11:58 +0300

    feat: Create views for commands_views.yaml

---
+++ app/utils/read_yaml.py
@@ -0,0 +1,20 @@
+import os
+
+from yaml import safe_load
+
+
+def read_views() -> dict[str, str]:
+    """
+    Функция чтения .yaml-файла с представлениями.
+
+    :return: Словарь представлениями.
+    """
+
+    data = {}
+
+    for files in os.walk(f'../../views'):
+        for file in files[2]:
+            if file.endswith('.yaml'):
+                data.update(safe_load(open('views/' + file, 'r', encoding='UTF8')))
+
+    return data
