83ef9bc59a7b1758c5a0320592786fc209d50bd3
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-11 01:34:10 +0300

    feat: Create function insert user.

--- app/database/CRUDs/insert_user.py
+++ app/database/CRUDs/insert_user.py
@@ -1,5 +1,27 @@
 from sqlalchemy import insert
 
+from app.validation.model_user import User
+from app.config.settings import session_maker, my_logger
+from app.database.models import *
 
-async def add_user() -> None:
-    pass
+
+async def add_user(user: User) -> None:
+    """
+    Асинхронная функция для работы с базой данных.
+    Добавляет пользователя в базу данных.
+
+    :param user: Объект класса User.
+    """
+
+    try:
+        async with session_maker() as session:
+            query = insert(SearchCompanion).values(user.model_dump())
+
+            await session.execute(query)
+
+            await session.commit()
+
+            return my_logger.info(f'Пользователь {user.telegram_id} добавлен в поиск собеседников!')
+
+    except ValueError:
+        my_logger.error(f'Ошибка валидации, при добавлении пользователя {user.telegram_id}')
