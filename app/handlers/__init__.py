c4982602dfe4411cc471a6db7e100b4ff8854453
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-11 00:09:56 +0300

    feat: Register chat router.

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 
 from .commands import router as router_commands
+from .chat import router as router_chat
 
 
 router = Router(name=__name__)
 
 
-router.include_routers(router_commands)
+router.include_routers(router_commands, router_chat)
