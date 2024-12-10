d3b3a802c6959d737e8e8e38416d32ccb8fc414c
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-11 00:22:07 +0300

    feat: Creation handler for click button 'start search'.

--- app/handlers/chat/handler_search.py
+++ app/handlers/chat/handler_search.py
@@ -1,5 +1,19 @@
 from aiogram import Router
+from aiogram.types import Message
+
+from app.filters.filters_chat import StartSearchFilter
+from app.config.settings import views
 
 
 router = Router(name=__name__)
 
+
+@router.message(StartSearchFilter())
+async def handler_start_search(message: Message) -> None:
+    """
+    Обработчик нажатия кнопки "🚀 Поиск случайного собеседника".
+
+    :param message: Объект класса Message,
+    """
+
+    await message.answer(text=views.get('search_msg'))
