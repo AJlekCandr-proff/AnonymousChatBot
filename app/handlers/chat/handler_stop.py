28bf9db3d414500f886ec367baff907505ba50a6
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-15 01:22:46 +0300

    feat: Create handler_stop.py.

--- app/handlers/chat/handler_stop.py
+++ app/handlers/chat/handler_stop.py
@@ -1,4 +1,17 @@
 from aiogram import Router
+from aiogram.types import Message
+
+from app.filters.filters_chat import StopChatFilter
 
 
 router = Router(name=__name__)
+
+
+@router.message(StopChatFilter())
+async def handler_stop_dialogue(message: Message) -> None:
+    """
+    Асинхронный обработчик нажатия кнопки "Стоп ❌".
+
+    :param message: Объект класса Message.
+    """
+    pass
