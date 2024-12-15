ff7f8047979125485cb6747ef25fb762bced6215
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-15 23:50:12 +0300

    feat: Addition and register router handler_interests.py

--- app/handlers/searching/__init__.py
+++ app/handlers/searching/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Router
 
+from .handler_interests import router as router_interests
+
 
 router = Router(name=__name__)
 
 
-router.include_routers()
+router.include_routers(router_interests)
