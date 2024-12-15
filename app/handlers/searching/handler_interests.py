4a014c2ff848f0c2e593f17257a59b1b43e2696c
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-16 00:07:35 +0300

    feat: Add handler for click button "📖 Интересы поиска".

--- app/handlers/searching/handler_interests.py
+++ app/handlers/searching/handler_interests.py
@@ -1,4 +1,19 @@
 from aiogram import Router
+from aiogram.types import Message
+
+from app.filters.filters_search import InterestsFilters
 
 
 router = Router(name=__name__)
+
+
+@router.message(InterestsFilters())
+async def handler_interests(message: Message) -> None:
+    """
+    Асинхронный обработчик нажатия кнопки "📖 Интересы поиска".
+    Присылает меню для выбора интересов поиска.
+
+    :param message: Объект класса Message.
+    """
+
+    pass
