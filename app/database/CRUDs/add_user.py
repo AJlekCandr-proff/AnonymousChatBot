ed670e070bf4ce20d5caf4c000ff79463f493d23
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-16 22:16:17 +0300

    feat: Move function "update_user_status" to update_user_status.py

--- app/database/CRUDs/add_user.py
+++ app/database/CRUDs/add_user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import insert, update
+from sqlalchemy import insert
 
 from app.validation.model_user import User
 from app.database.models.users import Profiles
@@ -24,25 +24,3 @@ async def add_new_user(user: User) -> None:
 
     except Exception as error:
         my_logger.error(f'При регистрации пользователя возникла ошибка: {error}')
-
-
-async def update_user_status(user: User, in_search_state: bool) -> None:
-    """
-    Асинхронная функция добавления пользователя в поиск собеседника.
-
-    :param user: Объект класса User.
-    :param in_search_state: Константа True или False.
-    """
-
-    try:
-        async with async_session() as session:
-            query = update(Profiles).values(in_search=in_search_state).where(Profiles.telegram_id == user.telegram_id)
-
-            await session.execute(query)
-
-            await session.commit()
-
-            return my_logger.info(f'Пользователь {user.telegram_id} начал поиск собеседника...')
-
-    except Exception as error:
-        my_logger.error(f'При начале поиска собеседника возникла ошибка: {error}')
