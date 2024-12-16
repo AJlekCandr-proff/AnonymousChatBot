d326d32365d80630b5f0d10080872ee2f6ea9150
AJlekCandr_proff <svznikov@yandex.ru>
2024-12-16 22:30:20 +0300

    feat: Update command start.

--- app/handlers/commands/handler_start.py
+++ app/handlers/commands/handler_start.py
@@ -2,7 +2,10 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-from app.config.settings import views
+from app.database.CRUDs.add_user import add_new_user
+from app.database.CRUDs.select_users import selects_user
+from app.validation.model_user import User
+from app.config.settings import views, my_logger
 from app.keyboards.menu_keyboards import start_menu
 
 
@@ -19,3 +22,12 @@ async def handler_start(message: Message) -> None:
     """
 
     await message.answer(text=views.get('start_msg'), reply_markup=start_menu())
+
+    if await selects_user(message.from_user.id) is None:
+        try:
+            new_user = User(telegram_id=message.from_user.id, full_name=message.from_user.full_name)
+
+        except ValueError:
+            return my_logger.error(f'Что-то пошло не так при добавлении нового пользователя {message.from_user.id}')
+
+        await add_new_user(new_user)
